8bdb93685d0df3fd0f3427464373c09186c7a2c7
Ivan Latypov <paramorphling@gmail.com>
2018-03-18 03:39:07 +0900

    Added mockup data Small stage with basic left/right controls Added gamepad UI elements

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed;
+
+    float inputAxis;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    bool spriteIsRight = true;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        ProcessInput();
+        ProcessSprite();
+    }
+
+    void ProcessInput()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+    }
+
+    void ProcessSprite()
+    {
+        spriteIsRight = inputAxis >= 0 ? true : false;
+        if (spriteIsRight == false)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * inputAxis * movementSpeed;
+    }
+}
