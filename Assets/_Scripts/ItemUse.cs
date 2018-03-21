91d4ff0f2011cd4b1e723e28c8c061909ee69757
Ivan Latypov <paramorphling@gmail.com>
2018-03-21 15:43:27 +0900

    Updated inventory to have items, made items drag'n'droppable

---
+++ Assets/_Scripts/ItemUse.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemUse : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public bool dragOnSurfaces = true;
+    public Canvas frame;
+
+    private GameObject m_DraggingIcon;
+    private RectTransform m_DraggingPlane;
+    private Image self;
+
+    void Start()
+    {
+        self = GetComponent<Image>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_DraggingIcon = new GameObject("icon");
+
+        m_DraggingIcon.transform.SetParent(frame.transform, false);
+        m_DraggingIcon.transform.SetAsLastSibling();
+
+        var image = m_DraggingIcon.AddComponent<Image>();
+
+        image.sprite = GetComponent<Image>().sprite;
+        image.SetNativeSize();
+
+        if (dragOnSurfaces)
+            m_DraggingPlane = transform as RectTransform;
+        else
+            m_DraggingPlane = frame.transform as RectTransform;
+
+        SetDraggedPosition(eventData);
+        self.enabled = false;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        if (m_DraggingIcon != null)
+            SetDraggedPosition(data);
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        if (dragOnSurfaces && data.pointerEnter != null && data.pointerEnter.transform as RectTransform != null)
+            m_DraggingPlane = data.pointerEnter.transform as RectTransform;
+
+        var rt = m_DraggingIcon.GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(m_DraggingPlane, data.position, data.pressEventCamera, out globalMousePos))
+        {
+            rt.position = globalMousePos;
+            rt.rotation = m_DraggingPlane.rotation;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (m_DraggingIcon != null)
+            Destroy(m_DraggingIcon);
+    }
+
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
+    }
+}
